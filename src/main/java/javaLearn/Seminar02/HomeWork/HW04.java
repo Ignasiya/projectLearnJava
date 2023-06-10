bf471426d1128ec365f202b0230ace7991ff510f
Ignasiya <ignasiya93@gmail.com>
2023-06-10 21:32:17 +0300

    HomeWork2

--- src/main/java/javaLearn/Seminar02/HomeWork/HW04.java
+++ src/main/java/javaLearn/Seminar02/HomeWork/HW04.java
@@ -14,7 +14,7 @@ public class HW04 {
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+            for (int j = 0; j < board[i].length; j++) {
                 System.out.print(board[i][j]);
             }
             System.out.println();
@@ -29,9 +29,38 @@ public class HW04 {
      */
     private static boolean isValidSudoku(char[][] board) {
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == '.') continue;
+                if (valid(board, i, j) == false) return false;
             }
         }
+        return true;
+    }
+
+    /**
+     * @param board доска судоку
+     * @param row строка
+     * @param col столбец
+     * @return проверка на валидность
+     */
+    private static boolean valid(char[][] board, int row, int col) {
+        // проверка строки
+        for (int i = 0; i < board.length; i++) {
+            if (i == row) continue;
+            if (board[i][col] == board[row][col]) return false;
+        }
+        // проверка столбца
+        for (int j = 0; j < board[0].length; j++) {
+            if (j == col) continue;
+            if (board[row][j] == board[row][col]) return false;
+        }
+        // проверка блока 3х3, долго думал, в итоге подсмотрел на stackoverflow
+        for (int i = (row / 3) * 3; i < (row / 3 + 1) * 3; i++) {
+            for (int j = (col / 3) * 3; j < (col / 3 + 1) * 3; j++) {
+                if (i == row && j == col) continue;
+                if (board[i][j] == board[row][col]) return false;
+            }
+        }
+        return true;
     }
 }
