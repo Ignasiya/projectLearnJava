7f0bd876ea59a6fd0239afd5124728ec8dd4e319
Ignasiya <ignasiya93@gmail.com>
2023-06-24 19:15:39 +0300

    Lesson1 PLO

--- src/main/java/JavaPLO/Lesson07/Ex004/Program.java
+++ src/main/java/JavaPLO/Lesson07/Ex004/Program.java
@@ -4,47 +4,44 @@ public class Program {
     public static void main(String[] args) {
         // #region Robot1 demo
 
-        //  Robot1 robot1 = new Robot1("name_1", 1)
-        
+        Robot1 robot1 = new Robot1("name_1", 1);
+        robot1.level = 100500;
+        System.out.printf("%s %d\n", robot1.name, robot1.level);
 
-        // robot1.level = 100500;
-        // System.out.printf("%s %d\n", robot1.name, robot1.level);
+        //#endregion
 
-         
-        // //#endregion
-       
-        // // #region processing
+        // #region processing
 
-        // robot1.startBIOS();
-        // robot1.startOS();
-        // robot1.sayHi();
+        robot1.startBIOS();
+        robot1.startOS();
+        robot1.sayHi();
 
-        // robot1.work();
+        robot1.work();
 
-        // robot1.sayBye();
-        // robot1.stopOS();
-        // robot1.stopBIOS();
+        robot1.sayBye();
+        robot1.stopOS();
+        robot1.stopBIOS();
 
-        // // #endregion
+        // #endregion
 
         System.out.println();
         // #endregion
 
-        // #region Robo2 demo
+        // #region Robot2 demo
 
-        // Robot2 robot2 = new Robot2("name_2", 1);
+        Robot2 robot2 = new Robot2("name_2", 1);
 
-        // System.out.printf("%s %d\n", 
-        // robot2.getName(), robot2.getLevel());
-        
-        // robot2.work();
-        // robot2.work();
-        // robot2.work();
-        // robot2.powerOff();
+        System.out.printf("%s %d\n",
+                robot2.getName(), robot2.getLevel());
+
+        robot2.work();
+        robot2.work();
+        robot2.work();
+        robot2.powerOff();
 
         // #endregion
 
-        //#region Robo3 demo
+        //#region Robot3 demo
 
         Robot3 robot3 = new Robot3("name_3");
         System.out.println("-------------");
@@ -55,7 +52,7 @@ public class Program {
         robot3.work();
 
         robot3.power();
-        
+
         //#endregion
     }
 
