80bb3ca2af82681d839aa1402c91e9389e3d1446
Ignasiya <ignasiya93@gmail.com>
2023-06-25 18:21:26 +0300

    HomeWork 7 PLO

--- src/main/java/JavaPLO/Seminar07/HomeWork/Shop/CategoryProduct.java
+++ src/main/java/JavaPLO/Seminar07/HomeWork/Shop/CategoryProduct.java
@@ -5,15 +5,6 @@ import java.util.List;
 
 public class CategoryProduct extends Shop {
 
-    public CategoryProduct(String name, List<Product> product) {
-        super(name);
-        super.setCatalog(product);
-    }
-
-    public CategoryProduct(List<Product> product) {
-        super.setCatalog(product);
-    }
-
     public CategoryProduct(String name) {
         super(name);
         super.setCatalog(new LinkedList<>());
