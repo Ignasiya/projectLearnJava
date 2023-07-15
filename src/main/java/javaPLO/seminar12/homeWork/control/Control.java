0819b44a078e8b259cd145b86f1d02e8f9d0d348
Ignasiya <ignasiya93@gmail.com>
2023-07-15 20:37:51 +0300

    HomeWork12

--- src/main/java/javaPLO/seminar12/homeWork/control/Control.java
+++ src/main/java/javaPLO/seminar12/homeWork/control/Control.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public interface Control {
 
     /**
-     * Open-closed principle открыт для расширения другими версиями view, закрыт для модификаций
+     * Open-closed principle открыт для расширения другими версиями control, закрыт для модификаций
      */
     public void onExecute(int item, Scanner scanner);
     public int size();
